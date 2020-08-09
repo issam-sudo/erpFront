e0c243e29b4ec9bbf43af6e6ee4c7410dbfaf239
issam-sudo <issambenkaroum95@gmail.com>
2020-08-09 22:57:54 +0100

    account preference final lng

--- src/app/preference/invoice/invoice.component.ts
+++ src/app/preference/invoice/invoice.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var translateFunc;
 @Component({
   selector: 'app-invoice',
   templateUrl: './invoice.component.html',
   styleUrls: ['./invoice.component.scss']
 })
 export class InvoiceComponent implements OnInit {
+  lng2: any;
 
   constructor() { }
+  vl: string;
 
+  lng: any;
+
+ 
   ngOnInit(): void {
+    translateFunc();
+    this.vl = 'frr';
+
+    console.log(this.vl);
+    
+  }
+
+  clickButton(e: string) {
+
+    this.lng  =  document.getElementById('selectlanguage1');
+    this.vl = e
+    console.log(this.vl);
   }
 
 }
