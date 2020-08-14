a49d0d9f3422fb990b61709d65a08145429c5b42
rabidi <rachida.abidi@gmail.com>
2020-08-14 14:30:12 +0200

    update

--- src/app/sale/delivery-note-page2/delivery-note-page2.component.ts
+++ src/app/sale/delivery-note-page2/delivery-note-page2.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl,  Validators } from '@angular/forms';
+import { FormControl, FormGroupDirective, NgForm, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 
-interface delai{
+interface delai {
 
   name: string;
   adress: string;
-  adressLivr:string;
+  adressLivr: string;
+}
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+
+  }
 }
 
 @Component({
@@ -23,19 +31,34 @@ export class DeliveryNotePage2Component implements OnInit {
 
   ]);
   users = [
-    { name: 'Hjije Youssef',adress:'Rue La fontaine 150 brussels'},
-    { name: 'Hjije Youssef',adress:'Rue La fontaine 150 brussels'},
-    { name: 'majed',adress:'Rue La fontaine 150 brussels'}
+    { name: 'Hjije Youssef', adress: 'Rue La fontaine 150 brussels', adressLivr: 'Rue La fontaine 150 brussels llllllll' },
+    { name: 'Hjije Youssef', adress: 'Rue La fontaine 150 brussels' },
+    { name: 'majd', adress: 'Rue La fontaine 150 brussels' }
   ];
 
+  champPersoForm = new FormGroup({
+    title: new FormControl('testx '),
+    content: new FormControl(),
+  });
 
-  filterUsers(event) {
-    const value = event.value;
-    console.log(value)
-}
+  champsPerso: any[];
+
+  selectedUser: any;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onSelectUserChange(event) {
+    this.selectedUser = event.value;
+  }
+  matcher = new MyErrorStateMatcher();
+
+  onChampPersoSubmit(){
+    console.log(this.champPersoForm.value);
+  }
+
+
 
 }
