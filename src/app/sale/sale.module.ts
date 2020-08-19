439ce3d7b9a28e81d37a0462226c322d6166581b
rabidi <rachida.abidi@gmail.com>
2020-08-19 10:49:06 +0200

    updete

--- src/app/sale/sale.module.ts
+++ src/app/sale/sale.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { SaleRoutingModule } from './sale-routing.module';
 import { SharedModule } from '../shared/shared.module';
-import { QuoteComponent } from './quote/quote.component';
+// import { QuoteComponent } from './quote/quote.component';
 import { NewProductServiceComponent } from './new-product-service/new-product-service.component';
 import { DeliveryNoteComponent } from './delivery-note/delivery-note.component';
 import { SaleHeaderComponent } from './sale-header/sale-header.component';
 
 
 @NgModule({
-  declarations: [ QuoteComponent, NewProductServiceComponent, SaleHeaderComponent],
+  declarations: [NewProductServiceComponent, SaleHeaderComponent],
   imports: [
     CommonModule,
     SaleRoutingModule,
